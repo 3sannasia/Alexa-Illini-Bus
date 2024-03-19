fc67bdb15008d1ad65b7cd9d8ab882437c04526c
Akash Sannasi <sannasi2@illinois.edu>
2024-03-18 21:28:31 -0500

    caching added

--- mtd_api.py
+++ mtd_api.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import requests
+import json
 
 load_dotenv()
 
@@ -11,10 +12,16 @@ key_url = f"key={mtd_api}"
 method = "getroutesbystop"
 base_url = f"https://developer.mtd.org/api/{version}/{data_format}/{method}?{key_url}"
 print(base_url)
+
 param = {
-    "stop_id": "it:1"
+    "stop_id": "it:1",
+    "changeset_id": "0"
 }
 
 response = requests.get(base_url, params = param)
+response_json = response.json()
+changeset_id = response_json["changeset_id"]
+param["changeset_id"] = changeset_id
+
 print(response.status_code)
-print(response.json())
+print(json.dumps(response_json, indent=4))
