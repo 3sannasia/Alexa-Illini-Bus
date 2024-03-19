f0e0fb0666434f19bf8dfc0bfccb04352e4120e3
Akash Sannasi <sannasi2@illinois.edu>
2024-03-18 22:14:23 -0500

    create api class

--- mtd_api.py
+++ mtd_api.py
@@ -3,25 +3,40 @@ from dotenv import load_dotenv
 import requests
 import json
 
-load_dotenv()
 
-mtd_api = os.getenv("MTD_API_KEY")
-version = "v2.2"
-data_format = "JSON"
-key_url = f"key={mtd_api}"
-method = "getroutesbystop"
-base_url = f"https://developer.mtd.org/api/{version}/{data_format}/{method}?{key_url}"
-print(base_url)
+class MTD_API:
+    def __init__(self):
+        load_dotenv()
+        self.api = os.getenv("MTD_API_KEY")
+        self.data_format = "JSON"
+        self.version = "v2.2"
+        self.base_url = "https://developer.mtd.org/api/{}/{}/{{}}?key={}".format(self.version, self.data_format, self.api)
+        self.changeset_id = None
+        self.cache = {}
 
-param = {
-    "stop_id": "it:1",
-    "changeset_id": "0"
-}
+    def get_routes_by_stop(self, stop_id: str) -> dict: 
+        request_method = "getroutesbystop"
+        param = {
+            "stop_id": stop_id,
+            "changeset_id": self.changeset_id
+        }
+        response = requests.get(self.base_url.format(request_method), params = param)
+        response_json = response.json()
+        if response.status_code == 202:
+            pass
+            # data not modified check in cache and return
+        elif response.status_code == 200:
+            changeset_id = response_json["changeset_id"]
+            param["changeset_id"] = changeset_id
+            return response_json
+        
+    def pretty_print(self, data: dict):
+        return json.dumps(data, indent=4)
+       
+if __name__ == "__main__":
+    mtd = MTD_API()
+    print(mtd.pretty_print(mtd.get_routes_by_stop("IT:1")))
 
-response = requests.get(base_url, params = param)
-response_json = response.json()
-changeset_id = response_json["changeset_id"]
-param["changeset_id"] = changeset_id
 
-print(response.status_code)
-print(json.dumps(response_json, indent=4))
+    
+
